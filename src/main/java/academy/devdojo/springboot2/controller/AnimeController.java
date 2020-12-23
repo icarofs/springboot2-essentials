711f2f9fda0bd936d7b1b7e046a7de812d6c32ea
Ícaro Ferreira de Souza <icaroferreira.souza@gmail.com>
2020-12-22 21:19:22 -0300

    adicionado camada de servico e repository, método GET

--- src/main/java/academy/devdojo/springboot2/controller/AnimeController.java
+++ src/main/java/academy/devdojo/springboot2/controller/AnimeController.java
@@ -1,6 +1,6 @@
 package academy.devdojo.springboot2.controller;
-
 import academy.devdojo.springboot2.domain.Anime;
+import academy.devdojo.springboot2.service.AnimeService;
 import academy.devdojo.springboot2.util.DateUtil;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
@@ -18,13 +18,13 @@ import java.util.List;
 public class AnimeController {
 
     private final DateUtil dateUtil;
+    private final AnimeService animeService;
 
     @GetMapping
-    public List<Anime> list(){
+    public List<Anime> list() {
         log.info(dateUtil.formatLocalDateTimeToDatabaseStyle(LocalDateTime.now()));
-        return List.of(new Anime("Boku No Hero"), new Anime("Berserk"));
+        return animeService.listAll();
     }
-
 
 
 }
